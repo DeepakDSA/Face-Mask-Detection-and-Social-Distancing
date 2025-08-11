432bc43cbe89512c2146ac2a6cd9726a749747f6
Deepak Kumar <159921932+DeepakDSA@users.noreply.github.com>
2025-08-12 03:14:05 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-# This command starts the Gunicorn server, which will run your app.py
-# It binds to the port specified by the PORT environment variable from Render.
-gunicorn --workers=2 --threads=4 --timeout 120 --bind 0.0.0.0:$PORT app:app
